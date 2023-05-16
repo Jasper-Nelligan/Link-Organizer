388fb7ab7428103809499dd137f107b95a0cd8eb
Jasper Nelligan <jnelligan@protonmail.com>
2023-05-16 15:47:50 -0600

    Add all elements to modal and style correctly
    Add all elements to modal and style correctly

--- link-organizer/src/Modal.js
+++ link-organizer/src/Modal.js
@@ -15,13 +15,47 @@ export default class Modal extends React.Component {
         <div className="modal" id="modal">
             <div className="modal-content">
                 <div className="form">
-                    <h2>Modal Window</h2>
-                    <div class="actions">
-                        <button class="toggle-button" onClick={this.onClose}>
-                            <a class="close">&times;</a>
-                        </button>
+                    <input className="course-input" type="text" name="course" placeholder="Course"/>
+
+                    <a className="close-button" onClick={this.onClose}>&times;</a>
+
+                    <label for="colors">Color : </label>
+                    <select id="color-selector" name="colors">
+                        <option value="red">Red</option>
+                        <option value="green">Green</option>
+                        <option value="blue">Blue</option>
+                        <option value="yellow">Yellow</option>
+                        <option value="orange">Orange</option>
+                        <option value="purple">Purple</option>
+                    </select>
+
+                    <div className="form-links" id="link0">
+                        <input className="link-name" type="text" name="link-name" placeholder="Title"/>
+                        <input className="link-input" type="text" name="link" placeholder="Link"/>
+                    </div>
+                    <div className="form-links" id="link1">
+                        <input className="link-name" type="text" name="link-name" placeholder="Title"/>
+                        <input className="link-input" type="text" name="link" placeholder="Link"/>
+                        <button type="button" className="remove-link-btn">Remove</button>
+                    </div>
+                    <div className="form-links" id="link2">
+                        <input className="link-name" type="text" name="link-name" placeholder="Title"/>
+                        <input className="link-input" type="text" name="link" placeholder="Link"/>
+                        <button type="button" className="remove-link-btn">Remove</button>
+                    </div>
+                    <div className="form-links" id="link3">
+                        <input className="link-name" type="text" name="link-name" placeholder="Title"/>
+                        <input className="link-input" type="text" name="link" placeholder="Link"/>
+                        <button type="button" className="remove-link-btn">Remove</button>
+                    </div>
+
+                    <button className="add-new-link">Add link</button>
+
+                    <div className="form-bottom">
+                        <button type="submit" className="submit-btn" id="submit-course">Create course</button>
                     </div>
                 </div>
+
             </div>
         </div>
     )
