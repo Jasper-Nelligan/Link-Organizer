195baa385b74746df8a4be2c9c42f29cde7ef4d1
Jasper Nelligan <jnelligan@protonmail.com>
2023-05-16 20:11:25 -0600

    Convert LinkField class to function

--- link-organizer/src/LinkField.js
+++ link-organizer/src/LinkField.js
@@ -1,23 +1,23 @@
 import React from "react";
 
-export default class LinkField extends React.Component {
-    render() {
-        console.log("Props is: ", this.props);
-        if (this.props.isFirstLink) {
-            console.log("isFirstLink")
-            return(
-                <div className="form-links" id="link1">
-                    <input className="link-name" type="text" name="link-name" placeholder="Title"/>
-                    <input className="link-input" type="text" name="link" placeholder="Link"/>
-                </div>
-            )
-        }
-        else return(
-                <div className="form-links" id="link1">
-                    <input className="link-name" type="text" name="link-name" placeholder="Title"/>
-                    <input className="link-input" type="text" name="link" placeholder="Link"/>
-                    <button type="button" className="remove-link-btn">Remove</button>
-                </div>
-            )
+function LinkField(props) {
+    console.log(props);
+    if (props.isFirstLink) {
+        console.log("isFirstLink")
+        return(
+            <div className="form-links" id="link1">
+                <input className="link-name" type="text" name="link-name" placeholder="Title"/>
+                <input className="link-input" type="text" name="link" placeholder="Link"/>
+            </div>
+        )
     }
+    else return(
+            <div className="form-links" id="link1">
+                <input className="link-name" type="text" name="link-name" placeholder="Title"/>
+                <input className="link-input" type="text" name="link" placeholder="Link"/>
+                <button type="button" className="remove-link-btn">Remove</button>
+            </div>
+        )
 }
+
+export default LinkField;
