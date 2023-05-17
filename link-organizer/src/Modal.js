5d3e87747173f70432d9df45ace0d816c52cbd96
Jasper Nelligan <jnelligan@protonmail.com>
2023-05-16 20:22:00 -0600

    Convert Modal.js to function

--- link-organizer/src/Modal.js
+++ link-organizer/src/Modal.js
@@ -2,48 +2,45 @@ import React from "react";
 import "./Modal.css";
 import LinkField from "./LinkField.js"
 
-export default class Modal extends React.Component {
-    onClose = e => {
-        this.props.onClose && this.props.onClose(e);
-      };   
-
-    render() {
-        if (!this.props.show){
-            return null;
-        }
-
-        return (
-            <div className="modal" id="modal">
-                <div className="modal-content">
-                    <div className="form">
-                        <input className="course-input" type="text" name="course" placeholder="Course"/>
-
-                        <a className="close-button" onClick={this.onClose}>&times;</a>
-
-                        <label htmlFor="colors">Color : </label> 
-                        <select id="color-selector" name="colors">
-                            <option value="red">Red</option>
-                            <option value="green">Green</option>
-                            <option value="blue">Blue</option>
-                            <option value="yellow">Yellow</option>
-                            <option value="orange">Orange</option>
-                            <option value="purple">Purple</option>
-                        </select>
-
-                        <LinkField isFirstLink={true}/>
-                        <LinkField isFirstLink={false}/>
-                        <LinkField isFirstLink={false}/>
-                        <LinkField isFirstLink={false}/>
-
-                        <button className="add-new-link">Add link</button>
-
-                        <div className="form-bottom">
-                            <button type="submit" className="submit-btn" id="submit-course">Create course</button>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        )
+function Modal(props) {
+    
+    if (!props.show){
+        return null;
     }
+
+    return (
+        <div className="modal" id="modal">
+            <div className="modal-content">
+                <div className="form">
+                    <input className="course-input" type="text" name="course" placeholder="Course"/>
+
+                    <a className="close-button" onClick={props.onClose()}>&times;</a>
+
+                    <label htmlFor="colors">Color : </label> 
+                    <select id="color-selector" name="colors">
+                        <option value="red">Red</option>
+                        <option value="green">Green</option>
+                        <option value="blue">Blue</option>
+                        <option value="yellow">Yellow</option>
+                        <option value="orange">Orange</option>
+                        <option value="purple">Purple</option>
+                    </select>
+
+                    <LinkField isFirstLink={true}/>
+                    <LinkField isFirstLink={false}/>
+                    <LinkField isFirstLink={false}/>
+                    <LinkField isFirstLink={false}/>
+
+                    <button className="add-new-link">Add link</button>
+
+                    <div className="form-bottom">
+                        <button type="submit" className="submit-btn" id="submit-course">Create course</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
 }
+
+export default Modal;
