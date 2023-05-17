cae76b6de4322f747e1b61a55f563e3e6030a69d
Jasper Nelligan <jnelligan@protonmail.com>
2023-05-16 20:04:10 -0600

    Format Modal.js

--- link-organizer/src/Modal.js
+++ link-organizer/src/Modal.js
@@ -7,43 +7,43 @@ export default class Modal extends React.Component {
         this.props.onClose && this.props.onClose(e);
       };   
 
-  render() {
-    if (!this.props.show){
-        return null;
-    }
+    render() {
+        if (!this.props.show){
+            return null;
+        }
 
-    return (
-        <div className="modal" id="modal">
-            <div className="modal-content">
-                <div className="form">
-                    <input className="course-input" type="text" name="course" placeholder="Course"/>
+        return (
+            <div className="modal" id="modal">
+                <div className="modal-content">
+                    <div className="form">
+                        <input className="course-input" type="text" name="course" placeholder="Course"/>
 
-                    <a className="close-button" onClick={this.onClose}>&times;</a>
+                        <a className="close-button" onClick={this.onClose}>&times;</a>
 
-                    <label htmlFor="colors">Color : </label> 
-                    <select id="color-selector" name="colors">
-                        <option value="red">Red</option>
-                        <option value="green">Green</option>
-                        <option value="blue">Blue</option>
-                        <option value="yellow">Yellow</option>
-                        <option value="orange">Orange</option>
-                        <option value="purple">Purple</option>
-                    </select>
+                        <label htmlFor="colors">Color : </label> 
+                        <select id="color-selector" name="colors">
+                            <option value="red">Red</option>
+                            <option value="green">Green</option>
+                            <option value="blue">Blue</option>
+                            <option value="yellow">Yellow</option>
+                            <option value="orange">Orange</option>
+                            <option value="purple">Purple</option>
+                        </select>
 
-                    <LinkField isFirstLink={true}/>
-                    <LinkField isFirstLink={false}/>
-                    <LinkField isFirstLink={false}/>
-                    <LinkField isFirstLink={false}/>
+                        <LinkField isFirstLink={true}/>
+                        <LinkField isFirstLink={false}/>
+                        <LinkField isFirstLink={false}/>
+                        <LinkField isFirstLink={false}/>
 
-                    <button className="add-new-link">Add link</button>
+                        <button className="add-new-link">Add link</button>
 
-                    <div className="form-bottom">
-                        <button type="submit" className="submit-btn" id="submit-course">Create course</button>
+                        <div className="form-bottom">
+                            <button type="submit" className="submit-btn" id="submit-course">Create course</button>
+                        </div>
                     </div>
-                </div>
 
+                </div>
             </div>
-        </div>
-    )
-  }
+        )
+    }
 }
