a3bfb80be063a39ee3f7bfc6ea6061488b3923ac
Jasper Nelligan <jnelligan@protonmail.com>
2023-05-16 20:29:26 -0600

    Convert App.js to function

--- link-organizer/src/App.js
+++ link-organizer/src/App.js
@@ -1,43 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 import Modal from './Modal';
 
-class App extends React.Component {
-    state = {
-        show: false
-      };
+function App() {
+    const [showModal, setShowModal] = useState(false);
 
-    showModal = e => {
-        this.setState({
-            show: true
-        });
-    };
+    return (
+        <>
+        <h1 className="center">Course Link Organizer</h1>
+        <h3 className="center">All your class links - One page</h3>
+        <div className="btn-container">
+            <button id="add-course-btn" data-modal="new-course-modal" onClick={e => {
+                setShowModal(true);
+                }}>Add course</button>
+        </div>
 
-    closeModal = e => {
-        this.setState({
-            show: false
-        })
-    }
+        <Modal onClose={setShowModal(false)} show={showModal}/>
 
-    render () {
-        return (
-            <>
-            <h1 className="center">Course Link Organizer</h1>
-            <h3 className="center">All your class links - One page</h3>
-            <div className="btn-container">
-                <button id="add-course-btn" data-modal="new-course-modal" onClick={e => {
-                    this.showModal();
-                    }}>Add course</button>
-            </div>
-
-            <Modal onClose={this.closeModal} show={this.state.show}/>
-
-            <p id="footer">
-                Questions, issues, or suggestions? Open an issue on 
-                <a href="https://github.com/Jasper-Nelligan/Link-Organizer" target="_blank"> github </a>
-                or email me at jnelligan@protonmail.com</p>
-            </>
-        )
-    }
+        <p id="footer">
+            Questions, issues, or suggestions? Open an issue on 
+            <a href="https://github.com/Jasper-Nelligan/Link-Organizer" target="_blank"> github </a>
+            or email me at jnelligan@protonmail.com</p>
+        </>
+    )
 }
 
 export default App;
